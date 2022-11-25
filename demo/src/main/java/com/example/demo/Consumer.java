4259a8ebf9eb57638205eb9a15443f4a34898f05
GassaraHEDI <hedi.gassara2@gmail.com>
2022-11-25 11:15:10 +0100

    modif TP7

--- demo/src/main/java/com/example/demo/Consumer.java
+++ demo/src/main/java/com/example/demo/Consumer.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.kafka.support.Acknowledgment;
@@ -15,9 +16,12 @@ public class Consumer  implements  ConsumerSeekAware {
     public static final String TOPIC = "trucks.position";
     private final Logger log = LoggerFactory.getLogger(TruckController.class);
 
+    @Autowired
+    ServiceTruck serviceTruck;
+
     @Override
     public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
-        callback.seekToTimestamp(assignments.keySet(), System.currentTimeMillis()-60*1000*60*60);
+        callback.seekToTimestamp(assignments.keySet(), System.currentTimeMillis()-1000*60*60);
     }
 
     @KafkaListener(id = "hgog-chat", topics = TOPIC)
@@ -25,5 +29,6 @@ public class Consumer  implements  ConsumerSeekAware {
     //    log.info("message received {}", position);
         TruckPositionRepository.addPosition(position);
         ack.acknowledge();
+        serviceTruck.positionChecker(position);
     }
 }
--- demo/src/main/java/com/example/demo/ServiceTruck.java
+++ demo/src/main/java/com/example/demo/ServiceTruck.java
@@ -1,19 +1,36 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
 public class ServiceTruck {
+    public static final String TOPIC = "trucks.alert";
+    private final Logger log = LoggerFactory.getLogger(ServiceTruck.class);
+
     @Autowired
     WebClient truckClient;
-
+    @Autowired
+    KafkaTemplate<String , TruckPosition > kafkaTemplate;
 
 
     public String post(Position position) {
         return truckClient.post().uri("/map").body(Mono.just(position), Position.class).retrieve().bodyToMono(String.class).block();
+    }
+
+    @Async
+    public void positionChecker(TruckPosition truckPosition){
+        int alert =  truckClient.post().uri("/position/check").body(Mono.just(truckPosition), TruckPosition.class).retrieve().bodyToMono(Integer.class).block();
+        if (alert > 0){
+            kafkaTemplate.send(TOPIC, String.valueOf(truckPosition.getTruckId()), truckPosition);
+            log.info("Alert =  "+String.valueOf(alert));
+        }
     }
 }
